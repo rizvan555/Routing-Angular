1c5ab1dace9582b8455ce94102ce27f57c44a51f
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-08-30 20:43:03 +0200

    created not-found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { C1Component } from './c1/c1.component';
 import { C2Component } from './c2/c2.component';
 import { C3Component } from './c3/c3.component';
 import { SharedModule } from './shared.module';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent, C1Component, C2Component, C3Component],
+  declarations: [AppComponent, C1Component, C2Component, C3Component, NotFoundComponent],
   imports: [AppRoutingModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/router.ts
+++ src/app/router.ts
@@ -2,5 +2,11 @@ import { Routes } from '@angular/router';
 import { c1Route } from './c1/c1.route';
 import { C2Route } from './c2/c2.route';
 import { C3Route } from './c3/c3.route';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [c1Route, C2Route, C3Route];
+export const routes: Routes = [
+  c1Route,
+  C2Route,
+  C3Route,
+  { path: '**', component: NotFoundComponent },
+];
