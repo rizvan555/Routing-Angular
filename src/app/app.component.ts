23eac0b98146f28cabb4dc74fa9b94006c634ed5
Rizvan <rizvan@MacBook-Air-2.fritz.box>
2023-08-30 20:00:39 +0200

    created routing iwth RouterModule and routerLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <h1>Routing</h1>
+    <a routerLink="/c1">C1 Component</a> ||
+    <a routerLink="/c2">C2 Component</a> ||
+    <a routerLink="/c3">C3 Component</a>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'routing-angular';
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { C1Component } from './c1/c1.component';
+import { C2Component } from './c2/c2.component';
+import { C3Component } from './c3/c3.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, C1Component, C2Component, C3Component],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'c1',
+        component: C1Component,
+      },
+      {
+        path: 'c2',
+        component: C2Component,
+      },
+      {
+        path: 'c3',
+        component: C3Component,
+      },
+    ]),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
